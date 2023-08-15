134b78e1cebcc058bf1d99d3345a15a71068e9d1
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-08-15 20:24:04 +0200

    add initial sync file placeholders

---
+++ accounts-hpc/bin/ldap-sync.py
@@ -0,0 +1,10 @@
+import bonsai
+from accounts_hpc import config
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ accounts-hpc/bin/web-sync.py
@@ -0,0 +1,9 @@
+from accounts_hpc import config
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
