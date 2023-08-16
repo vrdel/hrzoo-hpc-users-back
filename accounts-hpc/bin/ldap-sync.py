c80ab573f924620e12e06aeb9899192dfd78578c
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-08-16 11:54:05 +0200

    add logger module and handle invalid LDAP credentials

--- accounts-hpc/bin/ldap-sync.py
+++ accounts-hpc/bin/ldap-sync.py
@@ -1,18 +1,30 @@
 #!/usr/bin/env python
 
 import bonsai
+import sys
+
 from accounts_hpc.config import parse_config
+from accounts_hpc.log import Logger
 
 
 def main():
+    lobj = Logger(sys.argv[0])
+    logger = lobj.get()
+
     confopts = parse_config()
-    client = bonsai.LDAPClient(confopts['ldap']['server'])
-    client.set_credentials(
-            "SIMPLE",
-            user=confopts['ldap']['user'],
-            password=confopts['ldap']['password']
-        )
-    conn = client.connect()
+
+    try:
+        client = bonsai.LDAPClient(confopts['ldap']['server'])
+        client.set_credentials(
+                "SIMPLE",
+                user=confopts['ldap']['user'],
+                password=confopts['ldap']['password']
+            )
+        conn = client.connect()
+    except bonsai.errors.AuthenticationError as exc:
+        logger.error(exc)
+        raise SystemExit(1)
+
     print(conn.whoami())
 
     print(conn.search(f"ou=People,{confopts['ldap']['basedn']}", bonsai.LDAPSearchScope.ONE))
