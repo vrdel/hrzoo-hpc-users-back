fc3d721ea9aa95745eef1d54929f5175b1afbc79
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-08-20 21:02:19 +0200

    Add initial web-sync.py

--- accounts-hpc/bin/web-sync.py
+++ accounts-hpc/bin/web-sync.py
@@ -1,8 +1,20 @@
-from accounts_hpc import config
+#!/usr/bin/env python
+
+import sys
+
+from accounts_hpc.config import parse_config
+from accounts_hpc.log import Logger
+
+from sqlalchemy import create_engine
 
 
 def main():
-    pass
+    lobj = Logger(sys.argv[0])
+    logger = lobj.get()
+
+    confopts = parse_config()
+
+    engine = create_engine("sqlite://{}".format(confopts['db']['path']), echo=True)
 
 
 if __name__ == '__main__':
