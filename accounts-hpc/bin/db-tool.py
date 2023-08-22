ba607d611d9b001382966e9e4186b07d4e94ce75
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-08-22 12:08:17 +0200

    move DB handling operations to separate tool

--- accounts-hpc/bin/db-tool.py
+++ accounts-hpc/bin/db-tool.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys
+import argparse
 
 from accounts_hpc.config import parse_config
 from accounts_hpc.log import Logger
@@ -8,8 +9,13 @@ from accounts_hpc.db import Base
 
 from sqlalchemy import create_engine
 
-
 def main():
+    parser = argparse.ArgumentParser(description='DB tool that help with initial create and evolution of database')
+    parser.add_argument('--init', dest='init', action='store_true',
+            help='Create database with table structure')
+
+    args = parser.parse_args()
+
     lobj = Logger(sys.argv[0])
     logger = lobj.get()
 
@@ -17,6 +23,10 @@ def main():
 
     engine = create_engine("sqlite:///{}".format(confopts['db']['path']), echo=True)
 
+    import ipdb; ipdb.set_trace()
+
+    if args.init:
+        Base.metadata.create_all(engine)
 
 if __name__ == '__main__':
     main()
