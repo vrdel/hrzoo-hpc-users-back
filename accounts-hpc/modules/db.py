d04991753eac0213b64696d16ec304aadfcf8ce8
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-09-08 13:25:54 +0200

    username field renamed and extended

--- accounts-hpc/modules/db.py
+++ accounts-hpc/modules/db.py
@@ -23,7 +23,7 @@ class User(Base):
     __tablename__ = 'users'
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    username: Mapped[str] = mapped_column(String(8))
+    person_unique: Mapped[str] = mapped_column(String(128))
     first_name: Mapped[str] = mapped_column(String(20))
     last_name: Mapped[str] = mapped_column(String(40))
     person_mail: Mapped[str] = mapped_column(String(60))
