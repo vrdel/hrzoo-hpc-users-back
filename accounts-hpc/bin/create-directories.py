7095a03b3a59210f164747af16e56ac6e4f1efa9
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-09-21 17:31:01 +0200

    create shared suid project directories

--- accounts-hpc/bin/create-directories.py
+++ accounts-hpc/bin/create-directories.py
@@ -2,11 +2,13 @@
 
 import sys
 import os
+import stat
 
 from accounts_hpc.config import parse_config  # type: ignore
 from accounts_hpc.log import Logger  # type: ignore
 from accounts_hpc.db import Base, Project, User, SshKey  # type: ignore
 
+import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy import and_
 from sqlalchemy.orm import sessionmaker
@@ -52,12 +54,40 @@ def main():
                     logger.error(exc)
 
             if completed == len(confopts['usersetup']['userdirs_in']):
-                user.is_dir_created = True
-                logger.info(f"Created {dir + user.ldap_username} with perm {user.ldap_uid}:{user.ldap_gid}")
+                try:
+                    user.is_dir_created = True
+                    project_info = session.query(Project).filter(Project.ldap_gid == user.ldap_gid).one()
+                    logger.info(f"Created {dir + user.ldap_username} with perm {user.ldap_uid}:{user.ldap_gid} ({user.ldap_username}:{project_info.identifier})")
+                except sqlalchemy.exc.NoResultFound:
+                    logger.info(f"Created {dir + user.ldap_username} with perm {user.ldap_uid}:{user.ldap_gid}")
 
     projects = session.query(Project).all()
     for project in projects:
-        pass
+        if not project.is_dir_created:
+            completed = 0
+
+            for dir in confopts['usersetup']['groupdirs_in']:
+                try:
+                    os.mkdir(dir + project.identifier)
+                    os.chmod(dir + project.identifier, 0o2770)
+
+                except FileExistsError:
+                    pass
+                except (PermissionError, FileNotFoundError) as exc:
+                    logger.error(exc)
+                    break
+
+                try:
+                    userown = project.user[0]
+                    os.chown(dir + project.identifier, userown.ldap_uid, project.ldap_gid)
+                    completed += 1
+
+                except (PermissionError, OSError, FileNotFoundError) as exc:
+                    logger.error(exc)
+
+            if completed == len(confopts['usersetup']['groupdirs_in']):
+                project.is_dir_created = True
+                logger.info(f"Created {dir + project.identifier} with perm {userown.ldap_uid}:{project.ldap_gid} ({userown.ldap_username}:{project.identifier})")
 
     session.commit()
     session.close()
