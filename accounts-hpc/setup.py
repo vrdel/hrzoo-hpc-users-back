a4ccf8550f98412e2a949a874b2c7e6ff259fb00
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-09-25 23:55:26 +0200

    package all-flags-set.py

--- accounts-hpc/setup.py
+++ accounts-hpc/setup.py
@@ -38,7 +38,7 @@ setup(
                       'SQLAlchemy', 'sqlalchemy-json', 'Unidecode'],
     scripts=['bin/1-api-sync.py', 'bin/2-user-metadata-setup.py', 'bin/3-ldap-update.py',
              'bin/4-directories-create.py', 'bin/5-fairshare-update.py', 'bin/6-email-send.py',
-             'bin/7-mailinglist-subscribe.py', 'bin/db-tool.py'],
+             'bin/7-mailinglist-subscribe.py', 'bin/db-tool.py', 'bin/all-flags-set.py'],
     data_files=[
         ('bin/', glob.glob('bin/*')),
         ('etc/accounts-hpc', ['config/email-template-new-key.txt',
