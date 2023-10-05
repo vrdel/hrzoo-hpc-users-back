3ce98b1cae63281e7b47becfa9068e6761e3157a
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-10-05 16:13:52 +0200

    add get_ssh_key_fingerprint() from HZSI application

--- accounts-hpc/modules/utils.py
+++ accounts-hpc/modules/utils.py
@@ -1,6 +1,18 @@
 from typing import Any, Union
 
 
+def get_ssh_key_fingerprint(ssh_key):
+    # How to get fingerprint from ssh key:
+    # http://stackoverflow.com/a/6682934/175349
+    # http://www.ietf.org/rfc/rfc4716.txt Section 4.
+    import base64
+    import hashlib
+
+    key_body = base64.b64decode(ssh_key.strip().split()[1].encode('ascii'))
+    fp_plain = hashlib.md5(key_body).hexdigest()  # noqa: S303
+    return ':'.join(a + b for a, b in zip(fp_plain[::2], fp_plain[1::2]))
+
+
 def only_alnum(s: str) -> str:
     if '-' in s:
         s = s.split('-')
