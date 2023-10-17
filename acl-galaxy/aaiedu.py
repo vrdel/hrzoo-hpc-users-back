0a088dde46d5408d9bd0e5fd4132ab473fcd1f49
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-10-17 20:57:45 +0200

    raise correct exception for not allowed user

--- acl-galaxy/aaiedu.py
+++ acl-galaxy/aaiedu.py
@@ -4,6 +4,8 @@ Backend for OpenID AAIEDU
 
 from social_core.backends.open_id_connect import OpenIdConnectAuth
 from .hzsi_srce import hzsi_users
+from galaxy import exceptions
+import os
 
 
 class AAIEDUconn(OpenIdConnectAuth):
@@ -22,13 +24,27 @@ class AAIEDUconn(OpenIdConnectAuth):
         username_key = self.setting('USERNAME_KEY', default=self.USERNAME_KEY)
 
         logged_username = response.get('hrEduPersonUniqueID', None)
+
         if logged_username:
             logged_username = logged_username[0]
+
+        return {
+            'username': logged_username,
+            'fullname': response.get('name'),
+            'email': response.get('email'),
+            'first_name': response.get('givenName'),
+            'last_name': response.get('family_name')
+        }
+
+    def auth_allowed(self, response, details):
+        ret_parent = super(OpenIdConnectAuth, self).auth_allowed(response, details)
+        logged_username = response.get('hrEduPersonUniqueID', None)
+
+        if logged_username:
+            logged_username = logged_username[0]
+
         hzsiusers = hzsi_users()
         if logged_username in hzsiusers:
-            return {
-                'username': logged_username,
-                'email': response.get('email'),
-                'first_name': response.get('givenName'),
-                'last_name': response.get('family_name')
-            }
+            return ret_parent
+        else:
+            raise exceptions.AuthenticationFailed('User not registered')
