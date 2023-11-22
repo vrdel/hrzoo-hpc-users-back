fa71dd7c94f011da0d76707103a2d896529644f5
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-11-22 11:43:17 +0100

    add helper script for default LDAP config

---
+++ docker/ldap-server/bootstrap-ldap.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+slapadd -n 0 -F /etc/openldap/slapd.d -l /etc/openldap/slapd.ldif
+echo "olcTLSCertificateFile: /etc/openldap/certs/ldap.cert" >> /etc/openldap/slapd.d/cn=config.ldif
+echo "olcTLSCertificateKeyFile: /etc/openldap/certs/ldap.key" >> /etc/openldap/slapd.d/cn=config.ldif
+chown ldap:ldap -R /etc/openldap/slapd.d
+supervisorctl start openldap
+ldapadd -h localhost -x -w foobar -D "cn=Manager,dc=hpc,dc=srce,dc=hr" -f ~/ldap-conf/base/base.ldif
+ldapadd -h localhost -x -w foobar -D "cn=Manager,dc=hpc,dc=srce,dc=hr" -f ~/ldap-conf/base/group.ldif
+ldapadd -h localhost -x -w foobar -D "cn=Manager,dc=hpc,dc=srce,dc=hr" -f ~/ldap-conf/base/passwd.ldif
