422ccce8d7cb88436bdae323dcef2dc7f44ed385
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-12-03 22:07:31 +0100

    SendEmail task

--- accounts-hpc/bin/ah-6-email-send.py
+++ accounts-hpc/bin/ah-6-email-send.py
@@ -1,55 +1,16 @@
 #!/usr/bin/env python
 
 import sys
-
-from accounts_hpc.shared import Shared  # type: ignore
-from accounts_hpc.db import User  # type: ignore
-from accounts_hpc.emailsend import EmailSend  # type: ignore
-
 import argparse
-import signal
+
+from accounts_hpc.tasks.emailsend import SendEmail
 
 
 def main():
     parser = argparse.ArgumentParser(description="""Send email for newly opened user and added SSH keys""")
     args = parser.parse_args()
 
-    shared = Shared(sys.argv[0])
-    confopts = shared.confopts
-    logger = shared.log.get()
-    dbsession = shared.dbsession
-
-    users_opened = set()
-    users = dbsession.query(User).filter(User.mail_is_opensend == False).all()
-    for user in users:
-        email = EmailSend(logger, confopts, user.person_mail,
-                          username=user.ldap_username)
-        if email.send():
-            user.mail_is_opensend = True
-            users_opened.add(user.ldap_username)
-            logger.info(f"Send email for created account {user.ldap_username} @ {user.person_mail}")
-
-    users = dbsession.query(User).filter(User.mail_is_sshkeyadded == False).all()
-    for user in users:
-        # skip for new users that are just created
-        # as they will receive previous email
-        if user.ldap_username in users_opened:
-            user.mail_is_sshkeyadded = True
-            user.mail_name_sshkey = list()
-            continue
-        nmail = 0
-        for sshkeyname in user.mail_name_sshkey:
-            email = EmailSend(logger, confopts, user.person_mail,
-                              sshkeyname=sshkeyname)
-            if email.send():
-                nmail += 1
-                logger.info(f"Send email for added key {sshkeyname} of {user.ldap_username}")
-        if nmail == len(user.mail_name_sshkey):
-            user.mail_is_sshkeyadded = True
-            user.mail_name_sshkey = list()
-
-    dbsession.commit()
-    dbsession.close()
+    SendEmail(sys.argv[0], args).run()
 
 
 if __name__ == '__main__':
---
+++ accounts-hpc/modules/tasks/emailsend.py
@@ -0,0 +1,47 @@
+from accounts_hpc.shared import Shared  # type: ignore
+from accounts_hpc.db import User  # type: ignore
+from accounts_hpc.emailsend import EmailSend  # type: ignore
+
+import sys
+
+
+class SendEmail(object):
+    def __init__(self, caller, args):
+        shared = Shared(caller)
+        self.confopts = shared.confopts
+        self.logger = shared.log.get()
+        self.dbsession = shared.dbsession
+        self.args = args
+
+    def run(self):
+        users_opened = set()
+        users = self.dbsession.query(User).filter(User.mail_is_opensend == False).all()
+        for user in users:
+            email = EmailSend(self.logger, self.confopts, user.person_mail,
+                              username=user.ldap_username)
+            if email.send():
+                user.mail_is_opensend = True
+                users_opened.add(user.ldap_username)
+                self.logger.info(f"Send email for created account {user.ldap_username} @ {user.person_mail}")
+
+        users = self.dbsession.query(User).filter(User.mail_is_sshkeyadded == False).all()
+        for user in users:
+            # skip for new users that are just created
+            # as they will receive previous email
+            if user.ldap_username in users_opened:
+                user.mail_is_sshkeyadded = True
+                user.mail_name_sshkey = list()
+                continue
+            nmail = 0
+            for sshkeyname in user.mail_name_sshkey:
+                email = EmailSend(self.logger, self.confopts, user.person_mail,
+                                  sshkeyname=sshkeyname)
+                if email.send():
+                    nmail += 1
+                    self.logger.info(f"Send email for added key {sshkeyname} of {user.ldap_username}")
+            if nmail == len(user.mail_name_sshkey):
+                user.mail_is_sshkeyadded = True
+                user.mail_name_sshkey = list()
+
+        self.dbsession.commit()
+        self.dbsession.close()
