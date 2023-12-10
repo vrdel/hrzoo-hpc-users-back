bae1b46b2b18c1915ad7a4f9fda862af9ca1b662
Daniel Vrcic <daniel.vrcic@gmail.com>
2023-12-10 19:35:40 +0100

    initial refine of task emailsend for newly created account in case of project_email set

--- accounts-hpc/modules/tasks/emailsend.py
+++ accounts-hpc/modules/tasks/emailsend.py
@@ -15,33 +15,64 @@ class SendEmail(object):
 
     def run(self):
         users_opened = set()
-        users = self.dbsession.query(User).filter(User.mail_is_opensend == False).all()
-        for user in users:
-            email = EmailSend(self.logger, self.confopts, user.person_mail,
-                              username=user.ldap_username)
-            if email.send():
-                user.mail_is_opensend = True
-                users_opened.add(user.ldap_username)
-                self.logger.info(f"Send email for created account {user.ldap_username} @ {user.person_mail}")
+        if self.confopts['email']['project_email']:
+            users = self.dbsession.query(User).all()
+            for user in users:
+                for project in user.mail_project_is_opensend.keys():
+                    if user.mail_project_is_opensend[project] == False:
+                        email = EmailSend(self.logger, self.confopts, user.person_mail,
+                                          username=user.ldap_username, project=project)
+                        if email.send():
+                            user.mail_is_opensend = True
+                            users_opened.add(user.ldap_username)
+                            self.logger.info(f"Send email for created account {user.ldap_username} - {project} @ {user.person_mail}")
 
-        users = self.dbsession.query(User).filter(User.mail_is_sshkeyadded == False).all()
-        for user in users:
-            # skip for new users that are just created
-            # as they will receive previous email
-            if user.ldap_username in users_opened:
-                user.mail_is_sshkeyadded = True
-                user.mail_name_sshkey = list()
-                continue
-            nmail = 0
-            for sshkeyname in user.mail_name_sshkey:
+            users = self.dbsession.query(User).filter(User.mail_is_sshkeyadded == False).all()
+            for user in users:
+                # skip for new users that are just created
+                # as they will receive previous email
+                if user.ldap_username in users_opened:
+                    user.mail_is_sshkeyadded = True
+                    user.mail_name_sshkey = list()
+                    continue
+                nmail = 0
+                for sshkeyname in user.mail_name_sshkey:
+                    email = EmailSend(self.logger, self.confopts, user.person_mail,
+                                      sshkeyname=sshkeyname)
+                    if email.send():
+                        nmail += 1
+                        self.logger.info(f"Send email for added key {sshkeyname} of {user.ldap_username}")
+                if nmail == len(user.mail_name_sshkey):
+                    user.mail_is_sshkeyadded = True
+                    user.mail_name_sshkey = list()
+        else:
+            users = self.dbsession.query(User).filter(User.mail_is_opensend == False).all()
+            for user in users:
                 email = EmailSend(self.logger, self.confopts, user.person_mail,
-                                  sshkeyname=sshkeyname)
+                                  username=user.ldap_username)
                 if email.send():
-                    nmail += 1
-                    self.logger.info(f"Send email for added key {sshkeyname} of {user.ldap_username}")
-            if nmail == len(user.mail_name_sshkey):
-                user.mail_is_sshkeyadded = True
-                user.mail_name_sshkey = list()
+                    user.mail_is_opensend = True
+                    users_opened.add(user.ldap_username)
+                    self.logger.info(f"Send email for created account {user.ldap_username} @ {user.person_mail}")
+
+            users = self.dbsession.query(User).filter(User.mail_is_sshkeyadded == False).all()
+            for user in users:
+                # skip for new users that are just created
+                # as they will receive previous email
+                if user.ldap_username in users_opened:
+                    user.mail_is_sshkeyadded = True
+                    user.mail_name_sshkey = list()
+                    continue
+                nmail = 0
+                for sshkeyname in user.mail_name_sshkey:
+                    email = EmailSend(self.logger, self.confopts, user.person_mail,
+                                      sshkeyname=sshkeyname)
+                    if email.send():
+                        nmail += 1
+                        self.logger.info(f"Send email for added key {sshkeyname} of {user.ldap_username}")
+                if nmail == len(user.mail_name_sshkey):
+                    user.mail_is_sshkeyadded = True
+                    user.mail_name_sshkey = list()
 
         self.dbsession.commit()
         self.dbsession.close()
