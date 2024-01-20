7c1607d19105dca579aa84494e8caeb52066ae30
Daniel Vrcic <daniel.vrcic@gmail.com>
2024-01-20 10:59:37 +0100

    handle exceptions that might happen while setting flags

--- accounts-hpc/bin/ah-all-flags-set.py
+++ accounts-hpc/bin/ah-all-flags-set.py
@@ -18,46 +18,50 @@ async def main():
     parser = argparse.ArgumentParser(description="""Helper tool to set all flags to true to not send emails""")
     args = parser.parse_args()
 
-    stmt = select(Project)
-    projects = await dbsession.execute(stmt)
-    projects = projects.scalars().all()
+    try:
+        stmt = select(Project)
+        projects = await dbsession.execute(stmt)
+        projects = projects.scalars().all()
 
-    stmt = select(User)
-    users = await dbsession.execute(stmt)
-    users = users.scalars().all()
+        stmt = select(User)
+        users = await dbsession.execute(stmt)
+        users = users.scalars().all()
 
-    for project in projects:
-        if not project.is_dir_created:
-            project.is_dir_created = True
-        if not project.is_pbsfairshare_added:
-            project.is_pbsfairshare_added = True
+        for project in projects:
+            if not project.is_dir_created:
+                project.is_dir_created = True
+            if not project.is_pbsfairshare_added:
+                project.is_pbsfairshare_added = True
 
-    if not shared.confopts['ldap']['project_organisation']:
-        for user in users:
-            if not user.is_opened:
-                user.is_opened = True
-            if not user.is_dir_created:
-                user.is_dir_created = True
-            if not user.mail_is_opensend:
-                user.mail_is_opensend = True
-            if not user.mail_is_sshkeyadded:
-                user.mail_is_sshkeyadded = True
-                user.mail_name_sshkey = []
-    else:
-        for user in users:
-            if not user.is_opened:
-                user.is_opened = True
-            if not user.is_dir_created:
-                user.is_dir_created = True
-            if user.mail_project_is_opensend:
-                for project in user.mail_project_is_opensend.keys():
-                    user.mail_project_is_opensend[project] = True
-            if user.mail_project_is_sshkeyadded:
-                for project in user.mail_project_is_sshkeyadded.keys():
-                    user.mail_project_is_sshkeyadded[project] = True
+        if not shared.confopts['ldap']['project_organisation']:
+            for user in users:
+                if not user.is_opened:
+                    user.is_opened = True
+                if not user.is_dir_created:
+                    user.is_dir_created = True
+                if not user.mail_is_opensend:
+                    user.mail_is_opensend = True
+                if not user.mail_is_sshkeyadded:
+                    user.mail_is_sshkeyadded = True
+                    user.mail_name_sshkey = []
+        else:
+            for user in users:
+                if not user.is_opened:
+                    user.is_opened = True
+                if not user.is_dir_created:
+                    user.is_dir_created = True
+                if user.mail_project_is_opensend:
+                    for project in user.mail_project_is_opensend.keys():
+                        user.mail_project_is_opensend[project] = True
+                if user.mail_project_is_sshkeyadded:
+                    for project in user.mail_project_is_sshkeyadded.keys():
+                        user.mail_project_is_sshkeyadded[project] = True
 
-    await dbsession.commit()
-    await dbsession.close()
+        await dbsession.commit()
+        await dbsession.close()
+
+    except Exception as exc:
+        print(f"Error setting flags: {repr(exc)}")
 
 
 if __name__ == '__main__':
