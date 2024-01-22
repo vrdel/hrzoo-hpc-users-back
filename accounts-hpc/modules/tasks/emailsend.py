0ad0a109dd8b90c184b8cbe435175eebf50e1bd3
Daniel Vrcic <daniel.vrcic@gmail.com>
2024-01-22 17:15:13 +0100

    added _email_project_activate and _email_project_deactivate and fix _email_project_key

--- accounts-hpc/modules/tasks/emailsend.py
+++ accounts-hpc/modules/tasks/emailsend.py
@@ -18,8 +18,10 @@ class SendEmail(object):
         self.dbsession = shared.dbsession[caller]
         self.args = args
         self.daemon = daemon
+        self.users_opened = set()
+        self.users_project_opened = dict()
 
-    async def _email_project_account(self, users, users_opened):
+    async def _email_project_account(self, users):
         for user in users:
             for project in user.mail_project_is_opensend.keys():
                 if user.mail_project_is_opensend[project] == False:
@@ -27,18 +29,21 @@ class SendEmail(object):
                                       username=user.ldap_username, project=project)
                     if await email.send():
                         user.mail_project_is_opensend[project] = True
-                        users_opened.add(user.ldap_username)
+                        if not self.users_project_opened.get(project, False):
+                            self.users_project_opened[project] = list()
+                        self.users_project_opened[project].append(user.ldap_username)
                         self.logger.info(f"Send email for created account {user.ldap_username} - {project} @ {user.person_mail}")
 
-    async def _email_project_key(self, users, users_opened):
+    async def _email_project_key(self, users):
         for user in users:
-            # skip for new users that are just created
-            # as they will receive previous email
-            if user.ldap_username in users_opened:
-                for project in user.mail_project_is_sshkeyadded.keys():
+            already_opened = 0
+            for project in user.mail_project_is_sshkeyadded.keys():
+                if user.ldap_username in self.users_project_opened.get(project, []):
                     user.mail_project_is_sshkeyadded[project] = True
-                    user.mail_name_sshkey = list()
-                continue
+                    already_opened += 1
+            user_project = await user.awaitable_attrs.project
+            if already_opened == len(user_project):
+                user.mail_name_sshkey = list()
             nmail = 0
             for sshkeyname in user.mail_name_sshkey:
                 for project in user.mail_project_is_sshkeyadded.keys():
@@ -52,14 +57,38 @@ class SendEmail(object):
                 if nmail == len(user.mail_name_sshkey) * len(user.mail_project_is_sshkeyadded.keys()):
                     user.mail_name_sshkey = list()
 
-    async def _email_account(self, users, users_opened):
+    async def _email_account(self, users):
         for user in users:
             email = EmailSend(self.logger, self.confopts, user.person_mail,
                               username=user.ldap_username)
             if await email.send():
                 user.mail_is_opensend = True
-                users_opened.add(user.ldap_username)
+                self.users_opened.add(user.ldap_username)
                 self.logger.info(f"Send email for created account {user.ldap_username} @ {user.person_mail}")
+
+    async def _email_project_deactivate(self, users):
+        for user in users:
+            if user.mail_project_is_deactivated:
+                for proj in user.mail_project_is_deactivated:
+                    if not user.mail_project_is_deactivated[proj]:
+                        email = EmailSend(self.logger, self.confopts, user.person_mail,
+                                          username=user.ldap_username, project=proj, deactivated=True)
+                        if await email.send():
+                            user.mail_project_is_deactivated[proj] = True
+                            self.logger.info(f"Send email for deactivated account {user.ldap_username} - {proj} @ {user.person_mail}")
+
+    async def _email_project_activate(self, users):
+        for user in users:
+            if user.mail_project_is_activated:
+                for proj in user.mail_project_is_activated:
+                    if not user.mail_project_is_activated[proj]:
+                        email = EmailSend(self.logger, self.confopts, user.person_mail,
+                                          username=user.ldap_username, project=proj, activated=True)
+                        if await email.send():
+                            user.mail_project_is_activated[proj] = True
+                            user.mail_project_is_opensend[proj] = True
+                            user.mail_project_is_sshkeyadded[proj] = True
+                            self.logger.info(f"Send email for reactivated account {user.ldap_username} - {proj} @ {user.person_mail}")
 
     async def _email_activate(self, users):
         for user in users:
@@ -81,11 +110,11 @@ class SendEmail(object):
                 user.mail_is_deactivated = False
                 self.logger.info(f"Send email for deactivated account {user.ldap_username} @ {user.person_mail}")
 
-    async def _email_key(self, users, users_opened):
+    async def _email_key(self, users):
         for user in users:
             # skip for new users that are just created
             # as they will receive previous email
-            if user.ldap_username in users_opened:
+            if user.ldap_username in self.users_opened:
                 user.mail_is_sshkeyadded = True
                 user.mail_name_sshkey = list()
                 continue
@@ -102,14 +131,29 @@ class SendEmail(object):
 
     async def run(self):
         try:
-            users_opened = set()
             if self.confopts['email']['project_email']:
                 users = await self.dbsession.execute(select(User))
                 users = users.scalars().all()
 
                 coros = []
                 for chunk in chunk_list(users):
-                    coros.append(self._email_project_account(chunk, users_opened))
+                    coros.append(self._email_project_activate(chunk))
+                calrets = await asyncio.gather(*coros, return_exceptions=True)
+                exc_raised, exc = contains_exception(calrets)
+                if exc_raised:
+                    raise exc
+
+                coros = []
+                for chunk in chunk_list(users):
+                    coros.append(self._email_project_deactivate(chunk))
+                calrets = await asyncio.gather(*coros, return_exceptions=True)
+                exc_raised, exc = contains_exception(calrets)
+                if exc_raised:
+                    raise exc
+
+                coros = []
+                for chunk in chunk_list(users):
+                    coros.append(self._email_project_account(chunk))
                 calrets = await asyncio.gather(*coros, return_exceptions=True)
                 exc_raised, exc = contains_exception(calrets)
                 if exc_raised:
@@ -118,7 +162,7 @@ class SendEmail(object):
                 # await self.email_project_account(users, users_opened)
                 coros = []
                 for chunk in chunk_list(users):
-                    coros.append(self._email_project_key(chunk, users_opened))
+                    coros.append(self._email_project_key(chunk))
                 calrets = await asyncio.gather(*coros, return_exceptions=True)
                 exc_raised, exc = contains_exception(calrets)
                 if exc_raised:
@@ -155,7 +199,7 @@ class SendEmail(object):
 
                 coros = []
                 for chunk in chunk_list(users):
-                    coros.append(self._email_account(chunk, users_opened))
+                    coros.append(self._email_account(chunk))
                 calrets = await asyncio.gather(*coros, return_exceptions=True)
                 exc_raised, exc = contains_exception(calrets)
                 if exc_raised:
@@ -167,7 +211,7 @@ class SendEmail(object):
 
                 coros = []
                 for chunk in chunk_list(users):
-                    coros.append(self._email_key(chunk, users_opened))
+                    coros.append(self._email_key(chunk))
                 calrets = await asyncio.gather(*coros, return_exceptions=True)
                 exc_raised, exc = contains_exception(calrets)
                 if exc_raised:
