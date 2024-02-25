be668a9e802eb556acc9f2a90233062b34c65b5a
Daniel Vrcic <daniel.vrcic@gmail.com>
2024-02-25 10:50:36 +0100

    refine deploy-config.sh with needed suffix argument specified

--- accounts-hpc/config/deploy-config.sh
+++ accounts-hpc/config/deploy-config.sh
@@ -1,10 +1,44 @@
 #!/bin/bash
 
 IFS=" "
+suffix=""
 
-for file in $(echo "*.$1")
+usage()
+{
+	printf "Usage: %s [argument]\n" $(basename $0) >&2
+	printf "       [-s]            - suffix extension of config files that will be copied over\n" >&2
+	exit 2
+}
+
+if [[ $# == 0 ]]
+then
+    usage
+fi
+
+while getopts 'hs:' OPTION
 do
-	echo cp -f $file ${file%.$1}
-	cp -f $file ${file%.$1}
-    sudo chown root:root ../logrotate.d/*
+    case $OPTION in
+        s)
+            suffix=$OPTARG
+             ;;
+
+        h)
+            usage
+            ;;
+        ?)
+            usage
+            ;;
+    esac
 done
+
+let ok=0
+if [ ! -z "$suffix" ]
+then
+    for file in $(echo "*.${suffix}")
+    do
+        echo cp -f $file ${file%.${suffix}}
+        cp -f $file ${file%.${suffix}}
+        ok=1
+    done
+    sudo chown root:root ../logrotate.d/*
+fi
